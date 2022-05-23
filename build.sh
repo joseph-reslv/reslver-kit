925e04bdf42e8a900eea535b0f58cfd506a674c7
Joseph <joseph@reslv.io>
2022-05-23 11:32:16 +0800

    update: build

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ reslver_graph_generator_path="${root}/reslver-static-graph-exporter/reslver-grap
 reslver_graph_exporter_path="${root}/reslver-graph-exporter/sources/"
 
 echo "Clone submodules..."
+git submodule init
 git submodule update --recursive --remote
 
 echo "Moving: Relsver..."
