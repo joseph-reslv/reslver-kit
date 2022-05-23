ccb4b7b0c1e7c28a3b5cd2619eb133a6bf487516
Joseph <joseph@reslv.io>
2022-05-23 12:43:30 +0800

    update: build

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 import pkg_resources
 
-required = {"diagrams", "pyinstaller", "yaml"}
+required = {"diagrams", "pyinstaller"}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing = required - installed
 
